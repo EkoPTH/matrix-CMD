cf620b262ae94167d2864bf90a2a902619acccd2
EkoPTH <Cv47eko@gmail.com>
2021-08-10 20:57:59 +0700

    first commit

---
+++ matrix4.bat
@@ -0,0 +1,20 @@
+@echo off
+title AHHHHHH
+color 02
+echo This Matrix Affect was created by Superman
+echo This program will shut down your computer!
+echo The Matrix Effect begins in
+ping -n 2 127.0.0.1>nul
+echo 5
+ping -n 2 127.0.0.1>nul
+echo 4
+ping -n 2 127.0.0.1>nul
+echo 3
+ping -n 2 127.0.0.1>nul
+echo 2
+ping -n 2 127.0.0.1>nul
+echo 1
+
+:superman
+echo %random%%random%%random%%random%%random%%random%%random%%random%
+goto superman
